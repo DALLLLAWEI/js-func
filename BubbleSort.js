011f3dbeea2ad8ddff672d5ba0af8387f1923c27
DALLLLAWEI <37649700+DALLLLAWEI@users.noreply.github.com>
2022-10-23 20:01:21 +0800

    更新BubbleSort.js 测试函数
    更新 BubbleSort.js 测试函数比较两数组时未使用toString判断导致判断值为false

--- BubbleSort.js
+++ BubbleSort.js
@@ -25,12 +25,12 @@ var ensure = function(condition, error) {
 
 
 var test = function() {
-    ensure(bubbleSort([1,3,2,5,32,13,32]) === [1,2,3,5,13,32,32], '错误')
-    ensure(bubbleSort([1, 33, 22, 15, 32, 13, 32]) === [1, 13, 15, 22, 32, 32, 33], '错误')
-    ensure(bubbleSort([1, 213, 82, 05, 32, 13, 32]) === [1, 05, 3, 5, 13, 32, 32], '错误')
-    ensure(bubbleSort([1, 213, 82, 05, 32, 13, 32]) === [1, 5, 13, 32, 32, 82, 213], '错误')
-    ensure(bubbleSort([1, 353, 2, 531, 32, 13, 32]) === [1, 2, 13, 32, 32, 353, 531], '错误')
-    ensure(bubbleSort([1, 3, 2, 51, 32, 13, 32]) === [1, 2, 3, 13, 32, 32, 51], '错误')
+    ensure(bubbleSort([1,3,2,5,32,13,32]).toString() === [1,2,3,5,13,32,32].toString(), '错误')
+    ensure(bubbleSort([1, 33, 22, 15, 32, 13, 32]).toString() === [1, 13, 15, 22, 32, 32, 33].toString(), '错误')
+    ensure(bubbleSort([1, 213, 82, 05, 32, 13, 32]).toString() === [1, 05, 3, 5, 13, 32, 32].toString(), '错误')
+    ensure(bubbleSort([1, 213, 82, 05, 32, 13, 32]).toString() === [1, 5, 13, 32, 32, 82, 213].toString(), '错误')
+    ensure(bubbleSort([1, 353, 2, 531, 32, 13, 32]).toString() === [1, 2, 13, 32, 32, 353, 531].toString(), '错误')
+    ensure(bubbleSort([1, 3, 2, 51, 32, 13, 32]).toString() === [1, 2, 3, 13, 32, 32, 51].toString(), '错误')
 }
 
 test()
